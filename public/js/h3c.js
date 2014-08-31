40674bad1c20c49d284b8b5624a38801cff79bcf
Wu Donghui <wudonghui@Wu-Donghuis-MacBook-Pro.local>
2014-08-31 14:35:08 +0800

    demo

--- public/js/h3c.js
+++ public/js/h3c.js
@@ -4,7 +4,9 @@
 var m = {
   currentScene: 1,
   numberOfScenes: 3,
+  isTransition: false,
   isAnimating: false,
+  arrowTween: null,
   currentTimeline: null
 };
 
@@ -14,31 +16,48 @@ $(document).ready(function() {
   $.nonbounce();
 });
 
+
+
 window.onload = function() {
 
   // register swipe event
-  // $$('#viewport').swipeDown(function(event) {
-  //   // swipeDown();
-  //   console.log(event);
-  // });
+  $$('#viewport').swipeUp(swipeUp);
 
-  $$('#viewport').swipeUp(function(event) {
-    swipeUp(event);
-    // console.log(event.currentTarget);
+  // register playScene event
+  $('#viewport').on('playScene', playScene);
+
+  // init the first scene
+  $('#viewport').trigger('playScene', 'scene_1');
+
+  m.arrowTween = TweenMax.to('.arrow', 1, {
+    bottom: "15px",
+    opacity: 1,
+    repeat: -1,
+    yoyo: true
   });
 
 };
 
+function playScene(event, scene) {
+  // console.log('play scene', event, scene);
+  var tl = timelines[scene](function() {
+      m.isAnimating = true;
+    },
+    function() {
+      m.isAnimating = false;
+    });
+  tl.play();
+};
 
 function swipeUp(event) {
   // do nothing if currentScene is the last scene
-  if (m.isAnimating) {
-    console.log('timeline is animating');
+  if (m.isTransition || m.isAnimating) {
+    // console.log('transition or animation is running...');
     return false;
   }
 
   if (m.currentScene == m.numberOfScenes) {
-    console.log('scene reachs last page');
+    // console.log('scene reachs last page');
     return false;
   }
 
@@ -50,45 +69,104 @@ function swipeUp(event) {
   // setup timeline
   var tl = new TimelineLite({
     paused: true,
-    onStart: function(){
-      m.isAnimating = true;
+    onStart: function() {
+      m.isTransition = true;
     },
-    onComplete: function(){
-      m.isAnimating = false;
+    onComplete: function() {
+      m.isTransition = false;
+      $('#viewport').trigger('playScene', 'scene_' + m.currentScene);
     }
   });
 
-  tl.add(TweenMax.to(scene, 1.5, {
+  tl.add(TweenMax.to(scene, 1, {
     top: "-100%",
-    ease:Power2.easeInOut
   }), 'swipeUp');
 
-
-  tl.add(TweenLite.from(comingScene, 1.5, {
+  tl.add(TweenLite.from(comingScene, 1, {
     top: "100%",
-    ease:Power2.easeInOut
   }), 'swipeUp');
-
 
   tl.play('swipeUp');
-
   m.currentScene += 1;
 };
 
 
 
+// define timelines
+var timelines = {};
 
-// function swipeDown() {
+// scene_1
+timelines.scene_1 = function(onStart, onComplete) {
+  var tl = new TimelineMax({
+    paused: true,
+    onStart: onStart,
+    onComplete: onComplete
+  });
 
-//   if (currentScene == 0) return false;
+  tl.add(TweenLite.to('#scene_1 .envelop', 1, {
+    opacity: 1,
+    ease: Power1.easeIn,
+  }));
 
-//   console.log('move to scene ', currentScene - 1);
+  tl.add(TweenLite.to('#scene_1 .text', 1, {
+    opacity: 1,
+    ease: Power1.easeIn,
+  }));
 
-//   var currentSceneId = '#scene_' + (currentScene);
-//   var nextSceneId = '#scene_' + (currentScene - 1);
+  tl.add(TweenLite.fromTo('#scene_1 .icon', 1, {
+    scaleX: 0,
+    scaleY: 0
+  }, {
+    opacity: 1,
+    scaleX: 1,
+    scaleY: 1,
+    ease: Bounce.easeOut
+  }));
+  return tl;
+};
 
-//   $(currentSceneId).addClass('down').removeClass('active');
-//   $(nextSceneId).addClass('active').removeClass('up');
 
-//   currentScene -= 1;
-// }
+
+// scene_2
+timelines.scene_2 = function(onStart, onComplete) {
+  var tl = new TimelineMax({
+    paused: true,
+    onStart: onStart,
+    onComplete: onComplete
+  });
+
+  tl.add(TweenLite.fromTo('#scene_2 .text', 1, {
+    left: '0%',
+  }, {
+    left: '50%',
+    opacity: 1,
+    ease: Bounce.easeOut,
+  }));
+
+  tl.add(TweenLite.fromTo(['#scene_2 .yun', '#scene_2 .icon'], 1, {
+    scaleX: 0,
+    scaleY: 0
+  }, {
+    opacity: 1,
+    scaleX: 1,
+    scaleY: 1,
+    ease: Bounce.easeOut
+  }));
+
+
+  tl.add(TweenMax.to('#scene_2 .book', 5, {
+    bezier: [{
+      left: 100,
+      top: 250
+    }, {
+      left: 300,
+      top: 0
+    }, {
+      left: 500,
+      top: 400
+    }],
+    ease: Power1.easeInOut
+  }));
+
+  return tl;
+};
