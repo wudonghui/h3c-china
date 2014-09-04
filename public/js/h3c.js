c5d710f58fb21e3e56047776f1ae39d4011002e6
Wu Donghui <wudonghui@Wu-Donghuis-MacBook-Pro.local>
2014-09-04 15:43:25 +0800

    demo

--- public/js/h3c.js
+++ public/js/h3c.js
@@ -31,12 +31,20 @@ window.onload = function() {
   // init the first scene
   $('#viewport').trigger('playScene', 'scene_1');
 
-  m.arrowTween = TweenMax.to('.arrow', 1, {
+
+
+  var tl = new TimelineMax({
+    paused: true,
+  });
+
+  tl.to('.arrow', 1, {
     bottom: "15px",
     opacity: 1,
     repeat: -1,
     yoyo: true
   });
+
+  m.arrowTween = tl;
 
 };
 
@@ -47,6 +55,11 @@ function playScene(event, scene) {
     },
     function() {
       m.isAnimating = false;
+      if (m.arrowTween.isActive)
+        m.arrowTween.resume();
+      else
+        m.arrowTween.play();
+      $('.arrow').show();
     });
   tl.play();
 };
@@ -63,7 +76,7 @@ function swipeUp(event) {
     return false;
   }
 
-  console.log('transit from ', m.currentScene, ' to ', m.currentScene + 1);
+  // console.log('transit from ', m.currentScene, ' to ', m.currentScene + 1);
 
   var scene = '#scene_' + (m.currentScene);
   var comingScene = '#scene_' + (m.currentScene + 1);
@@ -73,6 +86,9 @@ function swipeUp(event) {
     paused: true,
     onStart: function() {
       m.isTransition = true;
+      if (m.arrowTween.isActive)
+        m.arrowTween.pause();
+      $('.arrow').hide();
     },
     onComplete: function() {
       m.isTransition = false;
@@ -92,13 +108,15 @@ function swipeUp(event) {
   // tl.play('swipeUp');
 
   tl.to(scene, 1, {
-    top: "-100%",
-    // ease: Power1.easeIn,
-  }).set(comingScene, {'visibility': 'visible'}, "-=1")
-  .from(comingScene, 1, {
-    top: "100%",
-    // ease: Power1.easeIn,
-  }, "-=1");
+      top: "-100%",
+      // ease: Power1.easeIn,
+    }).set(comingScene, {
+      'visibility': 'visible'
+    }, "-=1")
+    .from(comingScene, 1, {
+      top: "100%",
+      // ease: Power1.easeIn,
+    }, "-=1");
 
   tl.play();
 
@@ -126,19 +144,42 @@ timelines.scene_1 = function(onStart, onComplete) {
       autoAlpha: true,
       ease: Power1.easeIn,
     }, "-=1.5")
-    .set('#scene_1 .envelop_white', {opacity: 1})
+    .set('#scene_1 .envelop_white', {
+      opacity: 1
+    })
 
-    .set(".cover", {perspective:800})
-    .set(".cover", {transformStyle:"preserve-3d"})
-    .set(".coverback", {rotationX:-180, opacity: 1})
-    .set([".coverback", ".coverfront"], {backfaceVisibility:"hidden"})
-    .to('#scene_1 .cover', 3, {rotationX: 140})
+  .set(".cover", {
+      perspective: 800
+    })
+    .set(".cover", {
+      transformStyle: "preserve-3d"
+    })
+    .set(".coverback", {
+      rotationX: -180,
+      opacity: 1
+    })
+    .set([".coverback", ".coverfront"], {
+      backfaceVisibility: "hidden"
+    })
+    .to('#scene_1 .cover', 3, {
+      rotationX: 140
+    })
 
-    .set('#scene_1 .cover', {zIndex:1}, "-=1")
-    .set('#scene_1 .letter', {zIndex:2}, "-=1")
-    .set('#scene_1 .envelop', {zIndex:3}, "-=1")
-    .set('#scene_1 .letter', {opacity:1}, "-=1")
-    .to('#scene_1 .letter', 1.5, {top: '0%'}, "-=1")
+  .set('#scene_1 .cover', {
+      zIndex: 1
+    }, "-=1")
+    .set('#scene_1 .letter', {
+      zIndex: 2
+    }, "-=1")
+    .set('#scene_1 .envelop', {
+      zIndex: 3
+    }, "-=1")
+    .set('#scene_1 .letter', {
+      opacity: 1
+    }, "-=1")
+    .to('#scene_1 .letter', 1.5, {
+      top: '0%'
+    }, "-=1")
     // .to(['#scene_1 .envelop', '#scene_1 .cover'], 2, {top: '20%'}, "-=2");
 
   return tl;
@@ -179,13 +220,13 @@ timelines.scene_2 = function(onStart, onComplete) {
     scaleY: 1,
     ease: Bounce.easeOut
   })
-  
+
   .fromTo('#scene_2 .textbottom', 1.5, {
     right: '0%',
   }, {
     right: '10%',
     autoAlpha: true,
-    ease: Bounce.easeOut,
+    // ease: Bounce.easeOut,
   });
 
   return tl;
@@ -205,22 +246,22 @@ timelines.scene_3 = function(onStart, onComplete) {
   });
 
   tl.fromTo('#scene_3 .texttop', 1.5, {
-    top: '0%'
-  }, {
-    top: '10%',
-    autoAlpha: true,
-    // ease: Bounce.easeOut,
-  })
-  .fromTo('#scene_3 .textmiddle', 1.5, {
-    right: '0%'
-  }, {
-    right: '20%',
-    autoAlpha: true,
-    // ease: Bounce.easeOut,
-  })
-  .to('#scene_3 .korea', 1, {
-    autoAlpha: true
-  })
+      top: '0%'
+    }, {
+      top: '10%',
+      autoAlpha: true,
+      // ease: Bounce.easeOut,
+    })
+    .fromTo('#scene_3 .textmiddle', 1.5, {
+      right: '0%'
+    }, {
+      right: '20%',
+      autoAlpha: true,
+      // ease: Bounce.easeOut,
+    })
+    .to('#scene_3 .korea', 1, {
+      autoAlpha: true
+    })
 
   .fromTo('#scene_3 .gs1', 1.5, {
     scaleX: 0,
@@ -232,17 +273,39 @@ timelines.scene_3 = function(onStart, onComplete) {
     ease: Bounce.easeOut
   })
 
-  .fromTo('#scene_3 .plane', 1.5, {
+  .fromTo('#scene_3 .plane', 3, {
     scaleX: 0,
     scaleY: 0,
-    top: "0%"
   }, {
     autoAlpha: true,
     scaleX: 1,
     scaleY: 1,
-    top: "60%"
-    // ease: Bounce.easeOut
   })
+
+
+  .to('#scene_3 .plane', 3, {
+    bezier: {
+      // curviness: 1.25,
+      values: [{
+        left: "10%",
+        top: "0%"
+      }, {
+        left: "50%",
+        top: "10%"
+      }, {
+        left: "0%",
+        top: "40%"
+      }, {
+        left: "20%",
+        top: "50%"
+      }]
+    },
+
+  },
+
+    "-=3"
+  )
+
 
   .fromTo('#scene_3 .after', 1.5, {
     scaleX: 0,
@@ -275,8 +338,8 @@ timelines.scene_4 = function(onStart, onComplete) {
     onComplete: onComplete
   });
 
-  tl.to('#scene_4 .mountain', 5, {
-    autoAlpha: true,
+  tl.to('#scene_4 .mountain', 1, {
+    opacity: 1,
   })
 
   .fromTo('#scene_4 .textbottom1', 1.5, {
@@ -285,7 +348,7 @@ timelines.scene_4 = function(onStart, onComplete) {
     bottom: '20%',
     autoAlpha: true,
   })
-  
+
   .to('#scene_4 .textbottom2', 1, {
     autoAlpha: true
   })
@@ -326,6 +389,49 @@ timelines.scene_4 = function(onStart, onComplete) {
   //   autoAlpha: true
   // })
 
+
+  return tl;
+};
+
+
+
+timelines.scene_5 = function(onStart, onComplete) {
+  var tl = new TimelineMax({
+    paused: true,
+    onStart: onStart,
+    onComplete: onComplete
+  });
+
+  tl.fromTo('#scene_5 .texttop', 1.5, {
+    right: "0%",
+  }, {
+    right: "40%",
+    autoAlpha: true
+  })
+
+  tl.fromTo('#scene_5 .textmiddle', 1.5, {
+    right: "0%",
+  }, {
+    right: "20%",
+    autoAlpha: true
+  })
+
+  .fromTo('#scene_5 .rizhi', 1.5, {
+    scaleX: 0,
+    scaleY: 0,
+  }, {
+    autoAlpha: true,
+    scaleX: 1,
+    scaleY: 1,
+    ease: Bounce.easeOut
+  })
+
+  tl.fromTo('#scene_5 .textbottom', 1.5, {
+    left: "0%",
+  }, {
+    left: "30%",
+    autoAlpha: true
+  })
 
   return tl;
 };
